03be4d77b0be0ca272e2e33f8c5deca68e8f96f5
Murad Akhundov <Murad.Akhundov@devoxsoftware.com>
2022-07-02 18:28:31 +0400

    Add python-dotenv library.

--- src/stories/settings.py
+++ src/stories/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qdeu#^f^8#1gayp%#@0t((ql#5w)eanbux*qd4*8sm0=((zmy5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
