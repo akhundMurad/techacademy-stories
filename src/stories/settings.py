6caf45bb1a179745ba54c76193c506e1cd7741a8
Murad Akhundov <Murad.Akhundov@devoxsoftware.com>
2022-08-27 19:33:30 +0400

    Add middleware

--- src/stories/settings.py
+++ src/stories/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "users.middlewares.BlacklistMiddleware",
 ]
 
 ROOT_URLCONF = "stories.urls"
